0883b3fcd4d10a24496a8865181c7a0c362e41f4
MetaKaiserZen <edomontt.sepeda@gmail.com>
2021-10-08 19:23:53 -0400

    Commit Transferencias
    Disponibiliza la ruta '/transferencias'

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const url = require('url');
 
 const { insertar, consultar, editar, eliminar } = require('./consultas');
+const { transferir, movimiento } = require('./consultas');
 
 http
     .createServer(async (request, response) =>
@@ -89,14 +90,79 @@ http
 
         if (request.url.startsWith('/transferencia') && request.method === 'POST')
         {
-            //
+            let body = '';
+
+            request.on('data', (chunk) =>
+            {
+                body += chunk;
+            });
+
+            request.on('end', async () =>
+            {
+                const datos = Object.values(JSON.parse(body));
+
+                const { Pool } = require("pg");
+
+                const config =
+                {
+                    user: 'postgres',
+                    host: 'localhost',
+                    password: '12345',
+                    port: 5432,
+                    database: 'bancosolar',
+                    max: 20,
+                    idleTimeoutMillis: 30000,
+                    connectionTimeoutMillis: 2000,
+                }
+
+                const pool = new Pool(config);
+
+                pool.connect(async (error_conexion, client, release) =>
+                {
+                    await client.query('BEGIN');
+
+                    try
+                    {
+                        const descontar = "UPDATE usuarios SET balance = balance - " + datos[2] + " WHERE nombre = '" + datos[0] + "'";
+                        const descuento =  await client.query(descontar);
+
+                        const acreditar = "UPDATE usuarios SET balance = balance + " + datos[2] + " WHERE nombre = '" + datos[1] + "'";
+                        const acreditacion = await client.query(acreditar);
+
+                        const consulta = "INSERT INTO transferencias (emisor, receptor, monto, fecha) VALUES ((SELECT id FROM usuarios WHERE nombre ='" + datos[0] + "'), (SELECT id FROM usuarios WHERE nombre ='" + datos[1] + "'), " + datos[2] + ", NOW())";
+                        const resultado = await client.query(consulta);
+
+                        await client.query('COMMIT');
+                    }
+                    catch (e)
+                    {
+                        await client.query('ROLLBACK');
+
+                        console.log('Error código: ' + e.code);
+                        console.log('Detalle del error: ' + e.detail);
+                        console.log('Tabla originaria del error: ' + e.table);
+                        console.log('Restricción violada en el campo: ' + e.constraint);
+                    }
+
+                    release();
+                    pool.end();
+                });
+
+                response.statusCode = 201
+
+                response.end(JSON.stringify('OK'));
+            });
         }
 
         // Devuelve todas las transferencias almacenadas en la base de datos en formato de arreglo
 
         if (request.url.startsWith('/transferencias') && request.method === 'GET')
         {
-            //
+            const registros = await movimiento();
+
+            response.statusCode = 200;
+
+            response.end(JSON.stringify(registros));
         }
     })
     .listen(3000, console.log("Server ON"));
--- readme.md
+++ readme.md
@@ -13,3 +13,15 @@
 * **V1.0**
 
 * *Se agrega la ruta '/usuarios'*
+
+* **V1.0**
+
+* *Se agrega la ruta '/transferencias'*
+
+# Datos Adicionales
+
+* *Se agregó la función getTransferencias en la constante eliminarUsuario para actualizar transferencias actuales*
+
+* *Se modifició la función getTransferencias del archivo index.html para recorrer correctamente los datos enviados por el JSON utilizando para ello la Notación Literal*
+
+* *La transacción que realiza una transferencia necesita ser corregida para poder ser llamada como función directamente desde el archivo consultas.js. A pesar de que funciona correctamente se considera que no se utilizaron las mejores prácticas para ello, puesto que se vuelve a llamar a la conexión mediante Pool pero la transacción en sí funciona sin errores y con su restricción correspondiente*
